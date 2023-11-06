2d9e6391680d4310d009ebf9d2576491d388779d
nirbelelti <nirbelelti@gmail.com>
2023-11-06 18:01:20 +0100

    Add return type and actor to methods who return object

--- dynamic_sequens_diagram.py
+++ dynamic_sequens_diagram.py
@@ -1,5 +1,11 @@
 import json
 
+def has_returned_object_type(bytecode):
+    for instruction in bytecode:
+        if instruction["opr"] == "return" and instruction["type"] is not None:
+            return True, instruction["type"]
+    return False, None
+
 def create_sequence_diagram(data_dict):
 
     my_file = open("Diagrams/MyDynamicSequenceDiagram.puml", "w+")
@@ -9,7 +15,13 @@ def create_sequence_diagram(data_dict):
     for method in data_dict['methods']:
 
         method_name = method['name']
-        my_file.write( class_name+" -> " + class_name + " : "+ str(method_name)+"()\n")
+        bytecode = method["code"]["bytecode"]
+        returns_object, return_type = has_returned_object_type(bytecode)
+        calling_method = "actor" if returns_object else class_name
+        my_file.write( calling_method+" -> " + class_name + " : "+ str(method_name)+"()\n")
+
+        if returns_object:
+            my_file.write( calling_method + " <-- " + class_name + " : " + str(return_type) + "\n")
 
     my_file.write("@enduml")
     my_file.close()
