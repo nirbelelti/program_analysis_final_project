db6fc40f5cdfd85ddf042794d7c25f73659b0b7a
nirbelelti <nirbelelti@gmail.com>
2023-11-06 12:55:39 +0100

    rename method

--- dynamic_sequens_diagram.py
+++ dynamic_sequens_diagram.py
@@ -1,19 +1,18 @@
 import json
 
 def create_sequence_diagram(data_dict):
-    methods = list_of_methods(data_dict)
+    methods = stuck_of_executed_methods(data_dict)
     for method in methods:
 
         print('Method:', method)
 
-
-
-def list_of_methods(
+def stuck_of_executed_methods(
         data_dict):  # Since we are incrementing our interpretation over various methods, it creates a list of methods.
     methods = []
     for obj in data_dict['methods']:
         methods.append(obj['name'])
     return methods
+
 
 if __name__ == '__main__':
     with open('java/json_bytcode/Simple.json', 'r') as f:
