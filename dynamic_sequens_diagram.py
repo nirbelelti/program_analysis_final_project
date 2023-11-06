f0c4efeef121bee1f2b64293054ccd6fb84931a4
nirbelelti <nirbelelti@gmail.com>
2023-11-06 12:40:24 +0100

    Read json file and return list of methods

---
+++ dynamic_sequens_diagram.py
@@ -0,0 +1,22 @@
+import json
+
+def create_sequence_diagram(data_dict):
+    methods = list_of_methods(data_dict)
+    for method in methods:
+
+        print('Method:', method)
+
+
+
+def list_of_methods(
+        data_dict):  # Since we are incrementing our interpretation over various methods, it creates a list of methods.
+    methods = []
+    for obj in data_dict['methods']:
+        methods.append(obj['name'])
+    return methods
+
+if __name__ == '__main__':
+    with open('java/json_bytcode/Simple.json', 'r') as f:
+        data_dict = json.load(f)
+
+    create_sequence_diagram(data_dict)
