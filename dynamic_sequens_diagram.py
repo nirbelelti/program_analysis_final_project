8ac9c04d80f3aa00b334f41a54271e9cbf5f84b7
nirbelelti <nirbelelti@gmail.com>
2023-11-06 14:55:01 +0100

    return diagram structor with method called and class name

--- dynamic_sequens_diagram.py
+++ dynamic_sequens_diagram.py
@@ -5,10 +5,11 @@ def create_sequence_diagram(data_dict):
     my_file = open("Diagrams/MyDynamicSequenceDiagram.puml", "w+")
     my_file.write("@startuml\n")
 
-
+    class_name = data_dict["name"].replace("/", "_")  # solv problem with / in name in plantuml
     for method in data_dict['methods']:
+
         method_name = method['name']
-        my_file.write("class_name -> class_name :"+ str(method_name)+"()\n")
+        my_file.write( class_name+" -> " + class_name + " : "+ str(method_name)+"()\n")
 
     my_file.write("@enduml")
     my_file.close()
